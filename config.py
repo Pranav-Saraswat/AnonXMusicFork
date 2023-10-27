0f8fe87b49d1e6273114f7d84002bf5476a9ed24
Zaid <itsunknown122@gmail.com>
2023-10-27 18:20:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,11 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
 
+# Set a Playlist Limit 
 QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", "10"))
+
+LEAVE_TIME = int(getenv("LEAVE_TIME", "3600"))
+
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
